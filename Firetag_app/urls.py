2d9f8185f2e34c16b24d9b701815c657eeb1fcb6
Vlad Golubev <Nakmak1998@gmail.com>
2018-07-04 13:52:03 +0300

    Удалил нахрен старое

--- Firetag_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.views.generic import TemplateView
-
-
-urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="Firetag_app/index.html")), 
-]
