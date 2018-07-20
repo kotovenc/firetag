d5de725b3b61b7163820dcbc0b9b9b3b1476f3de
Vlad <Nakmak1998@gmail.com>
2018-07-20 19:30:24 +0300

    Правка карты

--- Firetag_app/templates/js/menu.js
+++ Firetag_app/templates/js/menu.js
@@ -8,4 +8,12 @@ $(document).ready(function() {
 				$('.mobile-tab').slideToggle("slow");
 			});
 
+	$('#more_btn').click(function(){
+	  $("#hidden").fadeIn("slow");
+	});  
+
+	$('.close_btn').click(function(){
+	  $("#hidden").fadeOut("slow");
+});  
 });
+
